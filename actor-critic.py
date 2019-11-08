3877779d21904ca89a24a82d4ec69c557a5ca9f5
Hyun Jae Cho <hyunjaecho1213@gmail.com>
2019-11-08 15:09:54 -0500

    Update actor-critic.py

--- actor-critic.py
+++ actor-critic.py
@@ -1,7 +1,5 @@
 import argparse
-import gym
 import numpy as np
-from itertools import count
 from collections import namedtuple
 
 import torch
@@ -14,8 +12,6 @@ import os
 import lgsvl
 import random
 import time
-
-# Cart Pole
 
 parser = argparse.ArgumentParser(description='PyTorch actor-critic example')
 parser.add_argument('--gamma', type=float, default=0.99, metavar='G',
@@ -77,155 +73,235 @@ optimizer = optim.Adam(model.parameters(), lr=3e-2)
 eps = np.finfo(np.float32).eps.item()
 
 
-def select_action(state):
-    state = torch.from_numpy(state).float()
-    probs, state_value = model(state)
+class Scenario():
+    def __init__(self):
+        '''start simulator, spawn EGO and NPC'''
+        ###################### simulator ######################
+        self.sim = lgsvl.Simulator(os.environ.get("SIMULATOR_HOST", "127.0.0.1"), 8181)
+        if self.sim.current_scene == "BorregasAve":
+          self.sim.reset()
+        else:
+          self.sim.load("BorregasAve")
 
-    # create a categorical distribution over the list of probabilities of actions
-    m = Categorical(probs)
+        ###################### EGO ######################
+        spawns = self.sim.get_spawn()
+        state = lgsvl.AgentState()
+        state.transform = spawns[0]
+        self.ego = self.sim.add_agent("Lincoln2017MKZ (Apollo 5.0)", lgsvl.AgentType.EGO, state)
 
-    # and sample an action using the distribution
-    action = m.sample()
+        ###################### NPC ######################
+        sx = spawns[0].position.x - 8
+        sy = spawns[0].position.y
+        sz = spawns[0].position.z + 150
 
-    # save to action buffer
-    model.saved_actions.append(SavedAction(m.log_prob(action), state_value))
+        state = lgsvl.AgentState()
+        state.transform = spawns[0]
+        state.transform.position.x = sx
+        state.transform.position.z = sz
 
-    # the action to take (left or right)
-    return action.item()
+        state.transform.rotation.y = 180.0
+        self.npc = self.sim.add_agent("Sedan", lgsvl.AgentType.NPC, state)
 
+        ###################### NPC waypoints ######################
+        self.waypoints = [ \
+            lgsvl.DriveWaypoint(position=lgsvl.Vector(13.81, -3.15, 63.50),
+                      angle=lgsvl.Vector(0, 180, 0),
+                      speed=10),
 
-def finish_episode():
-    """
-    Training code. Calcultes actor and critic loss and performs backprop.
-    """
-    R = 0
-    saved_actions = model.saved_actions
-    policy_losses = [] # list to save actor (policy) loss
-    value_losses = [] # list to save critic (value) loss
-    returns = [] # list to save the true values
+            lgsvl.DriveWaypoint(position=lgsvl.Vector(13.81, -2.2, 17),
+                      angle=lgsvl.Vector(0, 180, 0),
+                      speed=10),
 
-    # calculate the true value using rewards returned from the environment
-    for r in model.rewards[::-1]:
-        # calculate the discounted value
-        R = r + args.gamma * R
-        returns.insert(0, R)
+            lgsvl.DriveWaypoint(position=lgsvl.Vector(13.81, -1.12, -46.505),
+                      angle=lgsvl.Vector(0, 180, 0),
+                      speed=10),
+        ]
 
-    returns = torch.tensor(returns)
-    returns = (returns - returns.mean()) / (returns.std() + eps)
-
-    for (log_prob, value), R in zip(saved_actions, returns):
-        advantage = R - value.item()
-
-        # calculate actor (policy) loss
-        policy_losses.append(-log_prob * advantage)
-
-        # calculate critic (value) loss using L1 smooth loss
-        value_losses.append(F.smooth_l1_loss(value, torch.tensor([R])))
-
-    # reset gradients
-    optimizer.zero_grad()
-
-    # sum up all the values of policy_losses and value_losses
-    loss = torch.stack(policy_losses).sum() + torch.stack(value_losses).sum()
-
-    # perform backprop
-    loss.backward()
-    optimizer.step()
-
-    # reset rewards and action buffer
-    del model.rewards[:]
-    del model.saved_actions[:]
-
-def reset_environment():
-    '''start simulator, spawn EGO and NPC'''
-    ###################### simulator ######################
-    sim = lgsvl.Simulator(os.environ.get("SIMULATOR_HOST", "127.0.0.1"), 8181)
-    if sim.current_scene == "BorregasAve":
-      sim.reset()
-    else:
-      sim.load("BorregasAve")
-
-    ###################### EGO ######################
-    spawns = sim.get_spawn()
-    state = lgsvl.AgentState()
-    state.transform = spawns[0]
-    ego = sim.add_agent("Lincoln2017MKZ (Apollo 5.0)", lgsvl.AgentType.EGO, state)
+        self.npc.on_waypoint_reached(self.on_waypoint)
+        self.npc.follow(waypoints)
 
 
-    ###################### NPC ######################
-    # NPC's initial position
-    sx = spawns[0].position.x - 8
-    sy = spawns[0].position.y
-    sz = spawns[0].position.z + 150
+        ###################### Traffic light is kept green ######################
+        controllables = sim.get_controllables()
+        # Pick a traffic light of intrest
+        signal = controllables[2]
+        # Get current controllable states
+        print("\n# Current control policy:")
+        print(signal.control_policy)
+        # Create a new control policy
+        control_policy = "trigger=100;green=100;yellow=0;red=0;loop"
+        # Control this traffic light with a new control policy
+        signal.control(control_policy)
 
-    state = lgsvl.AgentState()
-    state.transform = spawns[0]
-    state.transform.position.x = sx
-    state.transform.position.z = sz
+        self.distance = 0
 
-    state.transform.rotation.y = 180.0
-    npc = sim.add_agent("Sedan", lgsvl.AgentType.NPC, state)
-    return sim
-
-def step(action):
-    '''
-    Input: action to take
-    Return: resulting state
-            reward earned
-            finished terminal state or not
-            others
-    '''
-    pass
-
-# def render():
+        self.R = 0
+        self.saved_actions = model.saved_actions
+        self.policy_losses = [] # list to save actor (policy) loss
+        self.value_losses = [] # list to save critic (value) loss
+        self.returns = [] # list to save the true values
 
 
-def main():
-    running_reward = 10
 
-    # run inifinitely many episodes
-    for i_episode in count(1):
+    def on_waypoint(self, agent, index):
+        print("=======")
+        print("waypoint {} reached".format(index))
 
-        # reset environment and episode reward
-        simulator = reset_environment()
-        ep_reward = 0
 
-        # for each episode, only run 9999 steps so that we don't
-        # infinite loop while learning
-        for t in range(1, 10000):
+    def select_action(self):
+        EGO_position = np.array([self.ego.state.position.x,
+                                self.ego.state.position.y,
+                                self.ego.state.position.z])
+        NPC_position = np.array([self.npc.state.position.x,
+                                self.npc.state.position.y,
+                                self.npc.state.position.z])
+        fog = lgsvl.WeatherState()
+
+
+    def calculate_ttc(self):
+        '''calculate the time to collision between EGO and NPC'''
+        approaching = False
+        dist_changed = 0
+
+        dist = abs(math.sqrt( (self.npc.state.position.x - self.ego.state.position.x)**2 + \
+                              (self.npc.state.position.y - self.ego.state.position.y)**2 + \
+                              (self.npc.state.position.z - self.ego.state.position.z)**2))
+        if self.distance == 0:
+            self.distance = dist
+            approaching = None
+        else:
+            dist_changed = self.distance - dist
+            self.distance = dist
+            approaching = dist_changed > 0
+
+        relative_speed = self.npc.state.speed - self.ego.state.speed
+
+        self.ttc = np.round(self.distance / relative_speed, 3)
+        
+        if approaching == None:
+            ttc_log = "timestep = 0"
+        elif approaching:
+            ttc_log = self.npc.uid.split("(Clone)")[0] + " " + str(self.ttc) + " seconds"
+        else:
+            if self.distance != 0:
+              ttc_log = self.npc.uid.split("(Clone)")[0] + " is moving away from EGO"
+            else:
+              ttc_log = self.npc.uid.split("(Clone)")[0] + " " + str(self.ttc) + " seconds"
+
+        print(ttc_log)
+
+
+    def step(self):
+        '''
+        Input: action to take
+            (which have already took effect by setting weather, time of day)
+        Return: resulting state
+                reward earned
+                finished in terminal state or not
+                others
+
+        In this function, we run the simulator with the set actions, 
+        and calculate the reward
+        '''
+        ###################### connect to bridge; run simulation ######################
+        # An EGO will not connect to a bridge unless commanded to
+        print("Bridge connected:", self.ego.bridge_connected)
+        # The EGO is now looking for a bridge at the specified IP and port
+        self.ego.connect_bridge("127.0.0.1", 9090)
+        print("Waiting for connection...")
+        while not self.ego.bridge_connected:
+          time.sleep(1)
+        print("Bridge connected:", self.ego.bridge_connected)
+        print("Initializing simulation")
+        sim.run(1)
+        input("Press Enter to run")
+
+        runtime = 30 #seconds
+        current_time = 0
+
+        while current_time < runtime:
+            self.sim.run(1)
+            self.calculate_ttc()
+            self.finish_episode()
+
+    def finish_episode(self):
+        """
+        Training code. Calcultes actor and critic loss, reward, and performs backprop.
+        """
+        # calculate the true value using rewards returned from the environment
+        for r in model.rewards[::-1]:
+            # calculate the discounted value
+            R = r + args.gamma * R
+            returns.insert(0, R)
+
+        returns = torch.tensor(returns)
+        returns = (returns - returns.mean()) / (returns.std() + eps)
+
+        for (log_prob, value), R in zip(saved_actions, returns):
+            advantage = R - value.item()
+
+            # calculate actor (policy) loss
+            policy_losses.append(-log_prob * advantage)
+
+            # calculate critic (value) loss using L1 smooth loss
+            value_losses.append(F.smooth_l1_loss(value, torch.tensor([R])))
+
+        # reset gradients
+        optimizer.zero_grad()
+
+        # sum up all the values of policy_losses and value_losses
+        loss = torch.stack(policy_losses).sum() + torch.stack(value_losses).sum()
+
+        # perform backprop
+        loss.backward()
+        optimizer.step()
+
+        # reset rewards and action buffer
+        del model.rewards[:]
+        del model.saved_actions[:]
+
+
+    def main():
+        running_reward = 10
+
+        # number of times to run scenario
+        for i_episode in range(1):
+
+            # reset environment and episode reward
+            simulator, ego, npc = reset_environment()
+
+            ep_reward = 0
 
             # select action from policy
-            simulator = select_action(state)
-            action = select_action(simulator)
+            sim.weather, time_of_day = select_action(simulator)
+            action = [sim.weather, time_of_day]
 
             # take the action
             state, reward, done, _ = step(action)
-
-            # if args.render:
-            #     render()
 
             model.rewards.append(reward)
             ep_reward += reward
             if done:
                 break
 
-        # update cumulative reward
-        running_reward = 0.05 * ep_reward + (1 - 0.05) * running_reward
+            # update cumulative reward
+            running_reward = 0.05 * ep_reward + (1 - 0.05) * running_reward
 
-        # perform backprop
-        finish_episode()
+            # perform backprop
+            finish_episode()
 
-        # log results
-        if i_episode % args.log_interval == 0:
-            print('Episode {}\tLast reward: {:.2f}\tAverage reward: {:.2f}'.format(
-                  i_episode, ep_reward, running_reward))
+            # log results
+            if i_episode % args.log_interval == 0:
+                print('Episode {}\tLast reward: {:.2f}\tAverage reward: {:.2f}'.format(
+                      i_episode, ep_reward, running_reward))
 
-        # check if we have "solved" the cart pole problem
-        if running_reward > env.spec.reward_threshold:
-            print("Solved! Running reward is now {} and "
-                  "the last episode runs to {} time steps!".format(running_reward, t))
-            break
+            # check if crash happens
+            if running_reward > env.spec.reward_threshold:
+                print("Crashed! Running reward is now {} and "
+                      "the last episode runs to {} time steps!".format(running_reward, t))
+                break
 
 
 if __name__ == '__main__':
-    main()
+    scenario = Scenario()
+    scenario.main()
